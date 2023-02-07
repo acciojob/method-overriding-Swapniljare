ab344976ddc0b1806af7acc8ec397707901d8029
Swapnil Jare <120264348+Swapniljare@users.noreply.github.com>
2023-02-07 20:38:15 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
-package com.driver;
-
-public class Main {
-  
+class B extends A{
+  void meth(){
+System.out.println("Method is overridden in Extendend class B");
+}
+ public static void main(String args[]){
+  B obj = new B();
+  obj.meth();
+  }
+}
+class A{
+  void meth(){
+System.out.println("Invoking method from class A");
 }
